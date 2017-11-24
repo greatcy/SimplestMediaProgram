a11bd66a9f7526248a4babd6249a70d9afe93fe4
eli <15989101149@163.com>
2017-11-24 06:16:49 -0800

    add test

--- SimpleYUVExample/src/main.c
+++ SimpleYUVExample/src/main.c
@@ -3,6 +3,10 @@
 #include "yuv.h"
 
 int main(){
-	simplest_yuv420_split("lena_256x256_yuv420p.yuv",256,256,1);
-	//printf("done\n");
+	//simplest_yuv420_split("lena_256x256_yuv420p.yuv",256,256,1);
+	//simplest_yuv444_split("lena_256x256_yuv444p.yuv",256,256,1); 
+	
+	//simplest_yuv420_gray("lena_256x256_yuv420p.yuv",256,256,1);	
+	simplest_yuv420_halfy("lena_256x256_yuv420p.yuv",256,256,1);
+	printf("done\n");
 }
